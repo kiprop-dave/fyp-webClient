25eefe5a9678bb154111463041ef0c49e37df775
kiprop-dave <davekip16@gmail.com>
2023-02-11 19:01:53 +0300

    added Loader as fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from "react";
 import Auth from "./components/Auth";
 import { Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader/Loader";
 
 const Login = lazy(() => import("./pages/login/Login"));
 const Readings = lazy(() => import("./pages/readings/Readings"));
@@ -8,7 +9,7 @@ const Readings = lazy(() => import("./pages/readings/Readings"));
 function App() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Auth />}>
